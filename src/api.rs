60862817f29e34fb537e06debe8a2bdf731d32ac
168ef0cb <0769082d>
2021-12-14 22:54:51 +0300

    Adds unit tests for main api methods

--- src/api.rs
+++ src/api.rs
@@ -32,7 +32,7 @@ fn _charge_fee(user: Principal, fee_to: Principal, fee: Nat) {
 
 #[update(name = "transfer")]
 #[candid_method(update)]
-async fn transfer(to: Principal, value: Nat) -> TxReceipt {
+fn transfer(to: Principal, value: Nat) -> TxReceipt {
     let from = ic::caller();
     let stats = State::get().stats_mut();
     if balance_of(from) < value.clone() + stats.fee.clone() {
@@ -49,7 +49,7 @@ async fn transfer(to: Principal, value: Nat) -> TxReceipt {
 
 #[update(name = "transferFrom")]
 #[candid_method(update, rename = "transferFrom")]
-async fn transfer_from(from: Principal, to: Principal, value: Nat) -> TxReceipt {
+fn transfer_from(from: Principal, to: Principal, value: Nat) -> TxReceipt {
     let owner = ic::caller();
     let from_allowance = allowance(from, owner);
     let stats = State::get().stats_mut();
@@ -92,7 +92,7 @@ async fn transfer_from(from: Principal, to: Principal, value: Nat) -> TxReceipt 
 
 #[update(name = "approve")]
 #[candid_method(update)]
-async fn approve(spender: Principal, value: Nat) -> TxReceipt {
+fn approve(spender: Principal, value: Nat) -> TxReceipt {
     let owner = ic::caller();
     let stats = State::get().stats_mut();
     if balance_of(owner) < stats.fee.clone() {
@@ -134,7 +134,7 @@ async fn approve(spender: Principal, value: Nat) -> TxReceipt {
 
 #[update(name = "mint")]
 #[candid_method(update, rename = "mint")]
-async fn mint(to: Principal, amount: Nat) -> TxReceipt {
+fn mint(to: Principal, amount: Nat) -> TxReceipt {
     let caller = ic::caller();
     let stats = State::get().stats_mut();
     if caller != stats.owner {
@@ -151,7 +151,7 @@ async fn mint(to: Principal, amount: Nat) -> TxReceipt {
 
 #[update(name = "burn")]
 #[candid_method(update, rename = "burn")]
-async fn burn(amount: Nat) -> TxReceipt {
+fn burn(amount: Nat) -> TxReceipt {
     let caller = ic::caller();
     let stats = State::get().stats_mut();
     let caller_balance = balance_of(caller);
@@ -343,5 +343,228 @@ fn get_user_approvals(who: Principal) -> Vec<(Principal, Nat)> {
     match allowances.get(&who) {
         Some(allow) => Vec::from_iter(allow.clone().into_iter()),
         None => Vec::new(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ic_kit::mock_principals::{alice, bob, john};
+    use ic_kit::MockContext;
+    use std::collections::HashSet;
+
+    fn init_context() -> &'static mut MockContext {
+        let context = MockContext::new().with_caller(alice()).inject();
+
+        crate::init(
+            "".into(),
+            "".into(),
+            "".into(),
+            8,
+            Nat::from(1000),
+            alice(),
+            Nat::from(0),
+            alice(),
+        );
+        context
+    }
+
+    #[test]
+    fn transfer_without_fee() {
+        init_context();
+        assert_eq!(Nat::from(1000), balance_of(alice()));
+
+        assert!(transfer(bob(), Nat::from(100)).is_ok());
+        assert_eq!(balance_of(bob()), Nat::from(100));
+        assert_eq!(balance_of(alice()), Nat::from(900));
+    }
+
+    #[test]
+    fn transfer_with_fee() {
+        MockContext::new().with_caller(alice()).inject();
+
+        crate::init(
+            "".into(),
+            "".into(),
+            "".into(),
+            8,
+            Nat::from(1000),
+            alice(),
+            Nat::from(100),
+            john(),
+        );
+
+        assert!(transfer(bob(), Nat::from(100)).is_ok());
+        assert_eq!(balance_of(bob()), Nat::from(100));
+        assert_eq!(balance_of(alice()), Nat::from(800));
+        assert_eq!(balance_of(john()), Nat::from(100));
+    }
+
+    #[test]
+    fn transfer_insufficient_balance() {
+        init_context();
+        assert_eq!(
+            transfer(bob(), Nat::from(1001)),
+            Err(TxError::InsufficientBalance)
+        );
+        assert_eq!(balance_of(alice()), Nat::from(1000));
+        assert_eq!(balance_of(bob()), Nat::from(0));
+    }
+
+    #[test]
+    fn transfer_wrong_caller() {
+        let context = init_context();
+        context.update_caller(bob());
+        assert_eq!(
+            transfer(bob(), Nat::from(100)),
+            Err(TxError::InsufficientBalance)
+        );
+        assert_eq!(balance_of(alice()), Nat::from(1000));
+        assert_eq!(balance_of(bob()), Nat::from(0));
+    }
+
+    #[test]
+    fn mint_by_owner() {
+        init_context();
+        assert!(mint(alice(), Nat::from(2000)).is_ok());
+        assert!(mint(bob(), Nat::from(5000)).is_ok());
+        assert_eq!(balance_of(alice()), Nat::from(3000));
+        assert_eq!(balance_of(bob()), Nat::from(5000));
+        assert_eq!(get_metadata().totalSupply, Nat::from(8000));
+    }
+
+    #[test]
+    fn mint_not_by_owner() {
+        let context = init_context();
+        context.update_caller(bob());
+        assert_eq!(mint(alice(), Nat::from(100)), Err(TxError::Unauthorized));
+    }
+
+    #[test]
+    fn burn_by_owner() {
+        init_context();
+        assert!(burn(Nat::from(100)).is_ok());
+        assert_eq!(balance_of(alice()), Nat::from(900));
+        assert_eq!(get_metadata().totalSupply, Nat::from(900));
+    }
+
+    #[test]
+    fn burn_too_much() {
+        init_context();
+        assert_eq!(burn(Nat::from(1001)), Err(TxError::InsufficientBalance));
+        assert_eq!(balance_of(alice()), Nat::from(1000));
+        assert_eq!(get_metadata().totalSupply, Nat::from(1000));
+    }
+
+    #[test]
+    fn burn_by_wrong_user() {
+        let context = init_context();
+        context.update_caller(bob());
+        assert_eq!(burn(Nat::from(100)), Err(TxError::InsufficientBalance));
+        assert_eq!(balance_of(alice()), Nat::from(1000));
+        assert_eq!(get_metadata().totalSupply, Nat::from(1000));
+    }
+
+    #[test]
+    fn transfer_from_with_approve() {
+        let context = init_context();
+        assert!(approve(bob(), Nat::from(500)).is_ok());
+        context.update_caller(bob());
+        assert!(transfer_from(alice(), john(), Nat::from(100)).is_ok());
+        assert_eq!(balance_of(alice()), Nat::from(900));
+        assert_eq!(balance_of(john()), Nat::from(100));
+        assert!(transfer_from(alice(), john(), Nat::from(100)).is_ok());
+        assert_eq!(balance_of(alice()), Nat::from(800));
+        assert_eq!(balance_of(john()), Nat::from(200));
+        assert!(transfer_from(alice(), john(), Nat::from(300)).is_ok());
+
+        assert_eq!(balance_of(alice()), Nat::from(500));
+        assert_eq!(balance_of(bob()), Nat::from(0));
+        assert_eq!(balance_of(john()), Nat::from(500));
+    }
+
+    #[test]
+    fn insufficient_allowance() {
+        let context = init_context();
+        assert!(approve(bob(), Nat::from(500)).is_ok());
+        context.update_caller(bob());
+        assert_eq!(
+            transfer_from(alice(), john(), Nat::from(600)),
+            Err(TxError::InsufficientAllowance)
+        );
+        assert_eq!(balance_of(alice()), Nat::from(1000));
+        assert_eq!(balance_of(john()), Nat::from(0));
+    }
+
+    #[test]
+    fn transfer_from_without_approve() {
+        let context = init_context();
+        context.update_caller(bob());
+        assert_eq!(
+            transfer_from(alice(), john(), Nat::from(600)),
+            Err(TxError::InsufficientAllowance)
+        );
+        assert_eq!(balance_of(alice()), Nat::from(1000));
+        assert_eq!(balance_of(john()), Nat::from(0));
+    }
+
+    #[test]
+    fn multiple_approves() {
+        init_context();
+        assert!(approve(bob(), Nat::from(500)).is_ok());
+        assert_eq!(get_user_approvals(alice()), vec![(bob(), Nat::from(500))]);
+
+        assert!(approve(bob(), Nat::from(200)).is_ok());
+        assert_eq!(get_user_approvals(alice()), vec![(bob(), Nat::from(200))]);
+
+        assert!(approve(john(), Nat::from(1000)).is_ok());
+
+        // Convert vectors to sets before comparing to make comparison unaffected by the element
+        // order.
+        assert_eq!(
+            HashSet::<&(Principal, Nat)>::from_iter(get_user_approvals(alice()).iter()),
+            HashSet::from_iter(vec![(bob(), Nat::from(200)), (john(), Nat::from(1000))].iter())
+        );
+    }
+
+    #[test]
+    fn approve_over_balance() {
+        let context = init_context();
+        assert!(approve(bob(), Nat::from(1500)).is_ok());
+        context.update_caller(bob());
+        assert!(transfer_from(alice(), john(), Nat::from(500)).is_ok());
+        assert_eq!(balance_of(alice()), Nat::from(500));
+        assert_eq!(balance_of(john()), Nat::from(500));
+
+        assert_eq!(
+            transfer_from(alice(), john(), Nat::from(600)),
+            Err(TxError::InsufficientBalance)
+        );
+        assert_eq!(balance_of(alice()), Nat::from(500));
+        assert_eq!(balance_of(john()), Nat::from(500));
+    }
+
+    #[test]
+    fn transfer_from_with_fee() {
+        let context = MockContext::new().with_caller(alice()).inject();
+
+        crate::init(
+            "".into(),
+            "".into(),
+            "".into(),
+            8,
+            Nat::from(1000),
+            alice(),
+            Nat::from(100),
+            bob(),
+        );
+        assert!(approve(bob(), Nat::from(1500)).is_ok());
+        assert_eq!(balance_of(bob()), Nat::from(100));
+        context.update_caller(bob());
+
+        assert!(transfer_from(alice(), john(), Nat::from(300)).is_ok());
+        assert_eq!(balance_of(bob()), Nat::from(200));
+        assert_eq!(balance_of(alice()), Nat::from(500));
+        assert_eq!(balance_of(john()), Nat::from(300));
     }
 }
