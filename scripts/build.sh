89d86856845137d22eaaeb7ffcc7f8ce54c3b0d4
Navid <yaa110@gmail.com>
2021-12-22 20:06:38 +0330

    CPROD-300 add factory to workspace

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,7 @@
 set -e
-cargo build --target wasm32-unknown-unknown --release
-ic-cdk-optimizer target/wasm32-unknown-unknown/release/ic20.wasm -o target/wasm32-unknown-unknown/release/ic20-opt.wasm
+cargo build -j`nproc || echo 1` --target wasm32-unknown-unknown --package token --release
+ic-cdk-optimizer target/wasm32-unknown-unknown/release/token.wasm -o src/factory/src/token.wasm
+cargo build -j`nproc || echo 1` --target wasm32-unknown-unknown --package factory --release
+ic-cdk-optimizer target/wasm32-unknown-unknown/release/factory.wasm -o target/wasm32-unknown-unknown/release/factory-opt.wasm
+cargo run -p factory > src/candid/factory.did
+cargo run -p token > src/candid/token.did
