623576841571f3d708a50991b1720e152c1273b0
0769082d <2a72003e>
2021-12-24 09:36:48 +0300

    Adds `inspect_message` function to prevent DDOS

--- src/token/src/api.rs
+++ src/token/src/api.rs
@@ -6,6 +6,7 @@ use common::types::Metadata;
 use ic_cdk_macros::*;
 use ic_kit::{ic, Principal};
 use ic_storage::IcStorage;
+use num_traits::ToPrimitive;
 use std::iter::FromIterator;
 
 mod dip20_meta;
@@ -83,6 +84,111 @@ fn post_upgrade() {
     *Balances::get().borrow_mut() = balances;
     *BiddingState::get().borrow_mut() = bidding_state;
     *AuctionHistory::get().borrow_mut() = auction_history;
+}
+
+/// This function checks if the canister should accept ingress message or not. We allow query
+/// calls for anyone, but update calls have different checks to see, if it's reasonable to spend
+/// canister cycles on accepting this call. Check the comments in this method for details on
+/// the checks for different methods.
+#[inspect_message]
+fn inspect_message() {
+    static PUBLIC_METHODS: [&str; 20] = [
+        "allowance",
+        "auctionInfo",
+        "balanceOf",
+        "biddingInfo",
+        "decimals",
+        "getAllowanceSize",
+        "getHolders",
+        "getMetadata",
+        "getTokenInfo",
+        "getTransaction",
+        "getTransactions",
+        "getUserApprovals",
+        "getUserTransactionAmount",
+        "getUserTransactions",
+        "historySize",
+        "logo",
+        "name",
+        "owner",
+        "symbol",
+        "totalSupply",
+    ];
+
+    static OWNER_METHODS: [&str; 8] = [
+        "mint",
+        "setAuctionPeriod",
+        "setFee",
+        "setFeeTo",
+        "setLogo",
+        "setMinCycles",
+        "setName",
+        "setOwner",
+    ];
+
+    static TRANSACTION_METHODS: [&str; 4] = ["approve", "burn", "transfer", "transferAndNotify"];
+
+    let method = ic_cdk::api::call::method_name();
+
+    let state = State::get();
+    let state = state.borrow();
+    let caller = ic_cdk::api::caller();
+
+    match &method[..] {
+        // These are query methods, so no checks are needed.
+        m if PUBLIC_METHODS.contains(&m) => ic_cdk::api::call::accept_message(),
+        m if OWNER_METHODS.contains(&m) => {
+            // These methods are allowed to be run only by the owner of the canister.
+            let owner = state.stats().owner;
+            if caller == owner {
+                ic_cdk::api::call::accept_message();
+            } else {
+                ic_cdk::println!("Owner method is called not by an owner. Rejecting.");
+            }
+        }
+        m if TRANSACTION_METHODS.contains(&m) => {
+            // These methods require the caller to have some balance, so we check if the caller
+            // has any token to their name.
+            let balances = Balances::get();
+            let balances = balances.borrow();
+            if balances.0.contains_key(&caller) {
+                ic_cdk::api::call::accept_message();
+            } else {
+                ic_cdk::println!("Transaction method is called not by a stakeholder. Rejecting.");
+            }
+        }
+        "notify" => {
+            // This method can only be done if the notification id is in the pending notifications
+            // list.
+            let notifications = state.notifications();
+            let (tx_id,) = ic_cdk::api::call::arg_data::<(Nat,)>();
+
+            // Unwrapping here is fine, for if this method panics, the call
+            // will be rejected, which is exactly what we need.
+            if notifications.contains(&tx_id.0.to_usize().unwrap()) {
+                ic_cdk::api::call::accept_message();
+            } else {
+                ic_cdk::println!("No pending notification with the given id. Rejecting.");
+            }
+        }
+        "runAuction" => {
+            // We allow running auction only to the owner or any of the cycle bidders.
+            let bidding_state = BiddingState::get();
+            let bidding_state = bidding_state.borrow();
+            if bidding_state.bids.contains_key(&caller) || caller == state.stats().owner {
+                ic_cdk::api::call::accept_message();
+            } else {
+                ic_cdk::println!("Auction run method is called not by owner or bidder. Rejecting.");
+            }
+        }
+        "bidCycles" => {
+            // We reject this message, because a call with cycles cannot be made through ingress,
+            // only from the wallet canister.
+        }
+        _ => {
+            ic_cdk::println!("The method called is not listed in the access checks. This is probably a code error.");
+        }
+    }
 }
 
 // These methods are not part of the standard and are added for convenience. They may be removed
