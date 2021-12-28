9d970cfb0a52a0e70193fea7fef3c947b34a334d
Maxim <maxim@gritsenko.biz>
2021-12-28 13:26:37 +0300

    Adds optional limit parameter for `transferWithFee` method

--- src/token/src/api/is20_transactions.rs
+++ src/token/src/api/is20_transactions.rs
@@ -1,6 +1,6 @@
 use crate::api::dip20_transactions::transfer;
 use crate::state::State;
-use crate::types::TxReceipt;
+use crate::types::{TxError, TxReceipt};
 use candid::{candid_method, Nat, Principal};
 use ic_cdk_macros::update;
 use ic_storage::IcStorage;
@@ -8,13 +8,23 @@ use ic_storage::IcStorage;
 /// Transfers the `value` amount of tokens to the `to` principal, using European style fee (the fee
 /// amount is added to the `value` amount, so the total amount reduced from the sender balance is
 /// `value + fee`).
+///
+/// If the `limit` value is provided and the amount `value + fee` exceeds this limit, an error
+/// `InsufficientAllowance` will be returned. This parameter can be used to guard against sudden
+/// large fee value change by the owner.
 #[update(name = "transferWithFee")]
 #[candid_method(update, rename = "transferWithFee")]
-fn transfer_with_fee(to: Principal, value: Nat) -> TxReceipt {
+fn transfer_with_fee(to: Principal, value: Nat, limit: Option<Nat>) -> TxReceipt {
     let state = State::get();
     let state = state.borrow();
     let stats = state.stats();
     let fee = stats.fee.clone();
+    if let Some(limit) = limit {
+        if value.clone() + fee.clone() > limit {
+            return Err(TxError::InsufficientAllowance);
+        }
+    }
+
     drop(state);
 
     transfer(to, value + fee)
@@ -34,7 +44,7 @@ mod tests {
         MockContext::new().with_caller(alice()).inject();
         canister_init_with_fee();
 
-        assert!(transfer_with_fee(bob(), Nat::from(200)).is_ok());
+        assert!(transfer_with_fee(bob(), Nat::from(200), None).is_ok());
         assert_eq!(balance_of(bob()), Nat::from(200));
         assert_eq!(balance_of(alice()), Nat::from(700));
         assert_eq!(balance_of(john()), Nat::from(100));
@@ -46,8 +56,21 @@ mod tests {
         canister_init_with_fee();
 
         assert_eq!(
-            transfer_with_fee(bob(), Nat::from(950)),
+            transfer_with_fee(bob(), Nat::from(950), None),
             Err(TxError::InsufficientBalance)
         );
+    }
+
+    #[test]
+    fn transfer_with_fee_limit() {
+        MockContext::new().with_caller(alice()).inject();
+        canister_init_with_fee();
+
+        assert_eq!(
+            transfer_with_fee(bob(), Nat::from(100), Some(Nat::from(150))),
+            Err(TxError::InsufficientAllowance)
+        );
+
+        assert!(transfer_with_fee(bob(), Nat::from(100), Some(Nat::from(200))).is_ok());
     }
 }
