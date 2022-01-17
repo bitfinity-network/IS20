9e89375d134178785983dab26b078c4c299e5b55
0a2b1a54 <>
2022-01-17 17:08:09 +0300

    Change the factory state to use IcStorage

--- src/factory/src/api.rs
+++ src/factory/src/api.rs
@@ -2,18 +2,19 @@
 //! Copyright  : 2021 InfinitySwap Team
 //! Stability  : Experimental
 
-use super::state::State;
+use crate::state::{get_token_bytecode, State};
 use candid::{candid_method, Principal};
 use common::types::Metadata;
 use ic_cdk_macros::*;
+use ic_storage::IcStorage;
 
-ic_helpers::init_api!(State);
+canisters::init_factory_api!(State, crate::state::get_token_bytecode());
 
 /// Returns the token, or None if it does not exist.
 #[query(name = "get_token")]
 #[candid_method(query, rename = "get_token")]
 async fn get_token(name: String) -> Option<Principal> {
-    State::get().factory.get(&name)
+    State::get().borrow().factory.get(&name)
 }
 
 /// Creates a new token.
@@ -24,10 +25,22 @@ pub async fn create_token(info: Metadata) -> Option<Principal> {
         return None;
     }
 
+    let state = State::get();
     let key = info.name.clone();
-    State::get()
-        .factory
-        .create(key, State::wasm(), (info,))
-        .await
-        .ok()
+
+    let create_token = {
+        let state = state.borrow();
+        let factory = &state.factory;
+        if let Some(existing) = factory.get(&key) {
+            return Some(existing);
+        }
+
+        factory.create(get_token_bytecode(), (info,))
+    };
+
+    let canister = create_token.await.ok()?;
+    let principal = canister.identity();
+    state.borrow_mut().factory.register(key, canister);
+
+    Some(principal)
 }
