6c2f00a9d624ce1aef73dbc286fcaa0664557696
Max Chamberlin <max@Maxs-MacBook-Pro.local>
2022-05-06 16:55:08 +0100

    stuff

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 set -e
 cargo build -j`nproc || echo 1` --target wasm32-unknown-unknown --package token --release
 ic-cdk-optimizer target/wasm32-unknown-unknown/release/token.wasm -o src/factory/src/token.wasm
-cargo build -j`nproc || echo 1` --target wasm32-unknown-unknown --package factory --release
-ic-cdk-optimizer target/wasm32-unknown-unknown/release/factory.wasm -o target/wasm32-unknown-unknown/release/factory-opt.wasm
-cargo run -p factory > src/candid/token-factory.did
+cargo build -j`nproc || echo 1` --target wasm32-unknown-unknown --package token-factory --release
+ic-cdk-optimizer target/wasm32-unknown-unknown/release/token-factory.wasm -o target/wasm32-unknown-unknown/release/factory-opt.wasm
+cargo run -p token-factory > src/candid/token-factory.did
 cargo run -p token > src/candid/token.did
