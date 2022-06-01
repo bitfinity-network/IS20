45d1f405913aef38a001517ee0b793417edfd573
Hagsteel <me@hagsteel.com>
2022-06-01 09:50:11 +0200

    added missed file (principal.rs)

---
+++ src/token/src/principal.rs
@@ -0,0 +1,47 @@
+use std::marker::PhantomData;
+use candid::Principal;
+
+use crate::types::{StatsData, TxError};
+use common::types::Metadata;
+
+/// Canister owner
+pub struct Owner;
+
+/// Any principal but the canister
+/// has isTestToken set to true
+pub struct TestNet;
+
+pub struct CheckedPrincipal<T>(Principal, PhantomData<T>);
+
+impl<T> CheckedPrincipal<T> {
+    pub fn inner(&self) -> Principal {
+        self.0
+    }
+}
+
+impl CheckedPrincipal<Owner> {
+    pub fn owner(stats: &StatsData) -> Result<Self, TxError> {
+        let caller = ic_kit::ic::caller();
+        if caller == stats.owner {
+            Ok(Self(caller, PhantomData))
+        } else {
+            Err(TxError::Unauthorized)
+        }
+    }
+
+    pub(crate) fn from_meta(meta: &Metadata) -> Self {
+        Self(meta.owner, PhantomData)
+    }
+}
+
+impl CheckedPrincipal<TestNet> {
+    pub fn test_user(stats: &StatsData) -> Result<Self, TxError> {
+        let caller = ic_kit::ic::caller();
+        if stats.is_test_token {
+            Ok(Self(caller, PhantomData))
+        } else {
+            Err(TxError::Unauthorized)
+        }
+    }
+}
+
