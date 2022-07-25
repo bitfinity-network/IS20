a0072fe96d939bebc99c4fe2c67812ec43034e86
Rumato Estorsky <rumatoest@users.noreply.github.com>
2022-07-25 10:25:27 +0300

    Update src/factory/build.rs
    Co-authored-by: Hagsteel <603835+hagsteel@users.noreply.github.com>

--- src/factory/build.rs
+++ src/factory/build.rs
@@ -4,25 +4,16 @@ fn main() {
     // Setting build tag from git repo
     let git_out = Command::new("git").args(["describe", "--tag"]).output();
 
-    let tag = match git_out {
-        Ok(o) => {
-            if o.status.success() {
-                String::from_utf8_lossy(&o.stdout).trim().to_string()
-            } else {
-                println!(
-                    "cargo:warning=Git command exited with error: {}",
-                    String::from_utf8_lossy(&o.stderr).to_string()
-                );
-                return;
-            }
+    match git_out {
+        Ok(o) if o.status.success() => {
+            let tag = String::from_utf8_lossy(&o.stdout).trim().to_string();
+            println!("cargo:warning=GIT tag extracted: {}", tag);
+            println!("cargo:rustc-env=CARGO_PKG_VERSION={}", tag);
         }
-        Err(e) => {
-            println!("cargo:warning=Can not extract git tag: {}", e);
-            return;
-        }
-    };
-
-    println!("cargo:warning=GIT tag extracted: {}", tag);
-
-    println!("cargo:rustc-env=CARGO_PKG_VERSION={}", tag);
+        Ok(o) => println!(
+            "cargo:warning=Git command exited with error: {}",
+            String::from_utf8_lossy(&o.stderr).to_string()
+        ),
+        Err(e) => println!("cargo:warning=Can not extract git tag: {}", e),
+    }```
 }
