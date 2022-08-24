d3caff90f8767110bbb94f7aa9c25fad43bc1568
Maxim <maxim@gritsenko.biz>
2022-08-24 11:16:59 +0300

    Removed unused error variants

--- src/token/api/src/error.rs
+++ src/token/api/src/error.rs
@@ -10,12 +10,6 @@ pub enum TxError {
     Unauthorized,
     #[error("Amount too small")]
     AmountTooSmall,
-    #[error("Fee exceeded limit {fee_limit}")]
-    FeeExceededLimit { fee_limit: Tokens128 },
-    #[error("Already actioned")]
-    AlreadyActioned,
-    #[error("Transaction does not exist")]
-    TransactionDoesNotExist,
     #[error("Bad fee {expected_fee}")]
     BadFee { expected_fee: Tokens128 },
     #[error("Insufficient funds : {balance}")]
@@ -32,12 +26,8 @@ pub enum TxError {
     AmountOverflow,
     #[error("Account is not found")]
     AccountNotFound,
-    #[error("{message}")]
-    GenericError { message: String },
     #[error("Claim not Allowed")]
     ClaimNotAllowed,
-    #[error("Temporary unavailable")]
-    TemporaryUnavailable,
 }
 
 #[derive(CandidType, Debug, PartialEq, Deserialize)]
@@ -55,9 +45,6 @@ pub enum TransferError {
 impl From<TxError> for TransferError {
     fn from(err: TxError) -> Self {
         match err {
-            TxError::FeeExceededLimit { fee_limit } => Self::BadFee {
-                expected_fee: fee_limit,
-            },
             TxError::BadFee { expected_fee } => Self::BadFee { expected_fee },
             TxError::InsufficientFunds { balance } => Self::InsufficientFunds { balance },
             TxError::TooOld { .. } => Self::TooOld,
@@ -65,7 +52,6 @@ impl From<TxError> for TransferError {
             TxError::Duplicate { duplicate_of } => Self::Duplicate {
                 duplicate_of: duplicate_of as u128,
             },
-            TxError::TemporaryUnavailable => Self::TemporarilyUnavailable,
             _ => TransferError::GenericError {
                 error_code: 500,
                 message: format!("{err}"),
