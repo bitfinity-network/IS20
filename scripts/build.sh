1d3ba740f4ca7bf3bb36f76f9e67f153217a550f
finiteops <119664751+finiteops@users.noreply.github.com>
2023-03-16 15:15:28 +0300

    Removed ic-cdk-optimizer

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 set -e
 cargo build --target wasm32-unknown-unknown --package is20-token-canister --features export-api --release
-ic-cdk-optimizer target/wasm32-unknown-unknown/release/is20-token-canister.wasm -o target/wasm32-unknown-unknown/release/token.wasm
+ic-wasm target/wasm32-unknown-unknown/release/is20-token-canister.wasm -o target/wasm32-unknown-unknown/release/token.wasm
 cargo build --target wasm32-unknown-unknown --package token-factory --features export-api --release
-ic-cdk-optimizer target/wasm32-unknown-unknown/release/token-factory.wasm -o target/wasm32-unknown-unknown/release/factory.wasm
+ic-wasm target/wasm32-unknown-unknown/release/token-factory.wasm -o target/wasm32-unknown-unknown/release/factory.wasm
 cargo run -p token-factory --features export-api > src/candid/token-factory.did
 cargo run -p is20-token-canister --features export-api > src/candid/token.did
